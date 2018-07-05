20fd757e7311c3e5f49b215a3621dc5ae3d0ae77
Matthias Schmieder <schmieder.matthias@gmail.com>
2018-07-05 21:01:33 +0200

    fixed dynamik build

--- cmake/FindAzureStorage.cmake
+++ cmake/FindAzureStorage.cmake
@@ -1,5 +1,14 @@
 include(FindPackageHandleStandardArgs)
 
+find_package(Threads REQUIRED)
+find_package(cpprestsdk REQUIRED)
+find_package(LibLZMA REQUIRED)
+find_package(Iconv REQUIRED)
+find_package(LibXml2 REQUIRED)
+find_package(JsonCpp REQUIRED)
+find_package(OpenSSL REQUIRED)
+find_package(Boost REQUIRED COMPONENTS log log_setup random system thread locale regex filesystem chrono date_time)
+
 find_path(AzureStorage_INCLUDE_DIR
   NAMES
   was/storage_account.h
@@ -23,7 +32,27 @@ if(AZURESTORAGE_FOUND)
   set_target_properties(Azure::Storage PROPERTIES
                         IMPORTED_LOCATION "${AzureStorage_LIBRARY}"
                         INTERFACE_INCLUDE_DIRECTORIES "${AzureStorage_INCLUDE_DIR}"
-                        IMPORTED_LINK_INTERFACE_LANGUAGES "CXX")
+                        IMPORTED_LINK_INTERFACE_LANGUAGES "CXX"
+                        IMPORTED_LINK_INTERFACE_LIBRARIES 
+                        Orthanc::Plugins
+                        Boost::thread
+                        Boost::log
+                        Boost::log_setup
+                        Boost::random
+                        Boost::system
+                        Boost::thread
+                        Boost::locale
+                        Boost::regex
+                        Boost::filesystem
+                        Boost::chrono
+                        Boost::date_time
+                        Threads::Threads
+                        OpenSSL::SSL
+                        OpenSSL::Crypto
+                        cpprestsdk::cpprest
+                        ${LIBLZMA_LIBRARIES}
+                        Iconv::Iconv
+                        ${LIBXML2_LIBRARY})
 
   set(AZURESTORAGE_LIBRARY "${AzureStorage_LIBRARY}")
   set(AZURESTORAGE_INCLUDE_DIRS "${AzureStorage_INCLUDE_DIR}")
