53490696f57f8f06b250eb4b6067e6589d60126e
Matthias Schmieder <schmieder.matthias@gmail.com>
2018-08-03 15:53:42 +0200

    added osx deployment

--- build.sh
+++ build.sh
@@ -3,21 +3,19 @@ set -e
 SOURCE_DIR=$(pwd)
 
 if [ -z "${ORTHANC_ROOT}" ];then
-  ORTHANC_ROOT=$(find $(pwd) -name Orthanc-* | head -n1)
+  ORTHANC_ROOT=$(find ${SOURCE_DIR} -name "Orthanc-*" | sort -r | head -n1)
 fi
 
 if [ -z "${BUILD_DIR}" ]; then
   BUILD_DIR=${SOURCE_DIR}/build/
 fi
 
+if [ -z "${VCPKG_ROOT}" ]; then
+   VCPKG_ROOT=${SOURCE_DIR}/vcpkg
+ fi
+
 if [ -z "${INSTALL_DIR}" ]; then
   INSTALL_DIR=${SOURCE_DIR}/install/
-fi
-
-if [ -z "${CMAKE_BINARY}" ]; then
-  cmake_exec=cmake
-else
-  cmake_exec=${CMAKE_BINARY}
 fi
 
 if [ -n "${CPPRESTSDK_ROOT}" ]; then
@@ -25,29 +23,47 @@ if [ -n "${CPPRESTSDK_ROOT}" ]; then
 fi
 
 # use the same cmake version that comes with VCPKG
+# use the same cmake version that comes with VCPKG
 if [ "$(uname)" == "Darwin" ]; then
-  osx_cmake_options="-DCMAKE_FIND_FRAMEWORK=LAST"
+  if [ -n "${USE_VCPKG}" ]; then
+    CMAKE_BINARY=$(find ${VCPKG_ROOT} -type f -perm +111 -name cmake)
+    vcpkg_cmake_options="-DUSE_VCPKG:BOOL=ON -DCMAKE_TOOLCHAIN_FILE:PATH=${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake"
+    osx_cmake_options="-DCMAKE_FIND_FRAMEWORK=LAST"
+  fi
+else
+  if [ -n "${USE_VCPKG}" ]; then
+    CMAKE_BINARY=$(find ${VCPKG_ROOT} -type f -executable -name cmake)
+  fi
+  vcpkg_cmake_options="-DVCPKG_TARGET_TRIPLET=x64-linux-fpic -DUSE_VCPKG:BOOL=ON -DCMAKE_TOOLCHAIN_FILE:PATH=${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake"
 fi
+
+if [ -z "${CMAKE_BINARY}" ]; then
+  CMAKE_BINARY=cmake
+fi
+
+
 
 mkdir -p ${BUILD_DIR}/{debug,release}
 mkdir -p ${INSTALL_DIR}
 
 cd ${BUILD_DIR}/debug
-${cmake_exec} ${SOURCE_DIR} -G"Unix Makefiles" \
+${CMAKE_BINARY} ${SOURCE_DIR} -G"Unix Makefiles" \
       -DORTHANC_ROOT:PATH="${ORTHANC_ROOT}" \
       -DCMAKE_INSTALL_PREFIX:PATH=${INSTALL_DIR} \
       -DCMAKE_BUILD_TYPE=Debug \
       ${osx_cmake_options} \
+      ${vcpkg_cmake_options} \
       ${CMAKE_ADDITIONAL_OPTIONS}
 
-${cmake_exec} --build . --config Debug --target install -- -j2
+${CMAKE_BINARY} --build . --config Debug --target install -- -j2
 
 cd ${BUILD_DIR}/release
-${cmake_exec} ${SOURCE_DIR} -G"Unix Makefiles" \
+${CMAKE_BINARY} ${SOURCE_DIR} -G"Unix Makefiles" \
       -DORTHANC_ROOT:PATH="${ORTHANC_ROOT}" \
       -DCMAKE_INSTALL_PREFIX:PATH=${INSTALL_DIR} \
       -DCMAKE_BUILD_TYPE=Release \
       ${osx_cmake_options} \
+      ${vcpkg_cmake_options} \
       ${CMAKE_ADDITIONAL_OPTIONS}
 
-${cmake_exec} --build . --config Release --target install -- -j2
+${CMAKE_BINARY} --build . --config Release --target install -- -j2
